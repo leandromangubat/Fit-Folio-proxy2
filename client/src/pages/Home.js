e9f4d9182aad30973429622afa1c5114f180deb1
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-08-17 12:55:03 -0400

    home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,30 @@
 import React from "react";
-// import { useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 
-// import SessionList from "../components/SessionList";
-// import SessionForm from "../components/SessionForm";
+import SessionList from "../components/SessionList";
+import SessionForm from "../components/SessionForm";
 
-// import { QUERY_SESSIONS } from "../utils/queries";
+import { QUERY_SESSIONS } from "../utils/queries";
 
 const Home = () => {
-  // const { loading, data } = useQuery(QUERY_SESSIONS);
-  // const sessions = data?.sessions || [];
+  const { loading, data } = useQuery(QUERY_SESSIONS);
+  const sessions = data?.sessions || [];
 
   return (
     <main>
       <div className="flex-row justify-center">
-        <div className="col-12 col-md-10 mb-3 p-3">
-          {/* <SessionForm />
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: "1px dotted #1a1a1a" }}
+        >
+          <SessionForm />
         </div>
         <div className="col-12 col-md-8 mb-3">
           {loading ? (
             <div>Loading...</div>
           ) : (
-            //<SessionList sessions={sessions} title="Your Sessions" />
-          )} */}
+            <SessionList sessions={sessions} title="Your Sessions" />
+          )}
         </div>
       </div>
     </main>
